95a6e0aba72ab3535640b8604b46c2cc2ec364d3
mercendan <mercendan@gmail.com>
2021-02-18 19:47:47 +0100

    Cluctering implemented

--- src/environment.cpp
+++ src/environment.cpp
@@ -53,6 +53,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // debug: draw only pcd
     //renderPointCloud(viewer, inputCloud, "inputCloud");
 
+    //segmentation
     // TODO:DONE Create point processor
     // create point processor on the stack
     ProcessPointClouds<pcl::PointXYZ> pointProcessor;
@@ -60,8 +61,26 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // create point processor on the heap
     // ProcessPointClouds<pcl::PointXYZ> * pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
     //std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor->SegmentPlane(inputCloud, 100, 0.2);
-    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+    // draw the obstacle(first) and the plane(second) points 
+    //renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(1, 1, 1));
+
+    // clustering
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
+    //std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor->Clustering(segmentCloud.first, 1.0, 3, 30);
+
+    int clusterId = 0;
+    std::vector<Color> colors = { Color(1,0,0), Color(1,1,0), Color(0,0,1) };
+
+    for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    {
+        std::cout << "cluster size ";
+        // write the cluster size (number of the points)
+        pointProcessor.numPoints(cluster);
+        //pointProcessor->numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId%colors.size()]);
+        ++clusterId;
+    }
 }
 
 
